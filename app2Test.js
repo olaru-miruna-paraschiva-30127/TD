24b41b4fe623bd8358c7ebe8d44cfd593cec4a71
olaru-miruna-paraschiva-30127 <99726825+olaru-miruna-paraschiva-30127@users.noreply.github.com>
2023-03-19 20:43:22 +0200

    Delete app2Test.js

--- app2Test.js
+++
@@ -1,9 +0,0 @@
-function test () {
-console.log ( sum (0) ==0? " Passed ":" Failed ");
-console.log ( sum (2) ==3? " Passed ":" Failed ");
- console.log ( sum (4) ==10? " Passed ":" Failed ") ;
- console.log ( sum () =="n is not a number"?" Passed ":" Failed ");
- console.log ( sum ("abcd") =="n is not a number"?" Passed ":" Failed ");
-console.log ( sum (false) =="n is not a number"?" Passed ":" Failed ");
- }
- test () ;
