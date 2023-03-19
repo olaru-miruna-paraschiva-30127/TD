4a301b68feb5256fbf93b05aecf933084885cc02
olaru-miruna-paraschiva-30127 <99726825+olaru-miruna-paraschiva-30127@users.noreply.github.com>
2023-03-19 20:43:36 +0200

    Delete app3Test.js

--- app3Test.js
+++
@@ -1,9 +0,0 @@
-function test () {
-console.log ( getFibonacci (2) =="[1,1]"? " Passed ":" Failed ");
-console.log ( getFibonacci (5) =="[1,1,2,3,5]"? " Passed ":" Failed ");
-console.log ( getFibonacci ("abcd") =="not allowed"? " Passed ":" Failed ");
-console.log ( getFibonacci (true) =="not allowed"? " Passed ":" Failed ");
-console.log ( getFibonacci (0) =="not allowed"? " Passed ":" Failed ");
-console.log ( getFibonacci (11) =="not allowed"? " Passed ":" Failed ");
- }
- test () ;
